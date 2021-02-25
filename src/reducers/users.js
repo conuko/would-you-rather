4bbc565b88b252c7ef56f01cbae538d5cf1b6dc9
Constantin <constantinu93@gmail.com>
2021-02-25 19:14:52 +0100

    fix: type error in index.js

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,10 +1,10 @@
-import { RECEIVE_USER } from '../actions/users';
+import { RECEIVE_USERS } from '../actions/users';
 import { ADD_QUESTION } from '../actions/questions';
 import { ADD_ANSWER } from '../actions/answers';
 
 export default function users (state = {}, action) {
     switch(action.type) {
-        case RECEIVE_USER :
+        case RECEIVE_USERS :
             return {
                 ...state,
                 ...action.users
