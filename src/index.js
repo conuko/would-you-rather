ab76be41a8dd516ee7b08a4fd91201bb3934fee4
Constantin <constantinu93@gmail.com>
2021-02-28 15:59:14 +0100

    feat: create components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import { createStore } from 'react';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 import middleware from './middleware';
