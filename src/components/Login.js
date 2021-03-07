2f33b34b31b82e4c3911bb0e986dd6a00e39bd0d
Constantin Unterkofler <constantinu93@gmail.com>
2021-03-07 17:37:17 +0100

    style: Login component with material-ui and styled-components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { setAuthedUser } from '../actions/authedUser';
+import styled from 'styled-components';
 
+import { StylesProvider } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
@@ -14,6 +16,39 @@ https://knowledge.udacity.com/questions/234946
 https://knowledge.udacity.com/questions/508001
 https://knowledge.udacity.com/questions/510658
 */
+
+const FormWrapper = styled.div`
+    position: relative;
+`;
+
+const StyledButton = styled(Button)`
+    margin: 20px auto;
+    background-color: #6772e5;
+    color: #fff;
+    box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+    padding: 7px 14px;
+    &:hover {
+    background-color: #5469d4;
+    }
+`;
+
+const StyledFormControl = styled(FormControl)`
+  width: 300px;
+  margin: 0 auto;
+  padding: 10px;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const H3 = styled.h3`
+    font-size: 54px;
+    font-weight: bold;
+    text-align: center;
+    padding-top: 30px;
+    padding-bottom: 30px;
+`;
 
 class Login extends Component {
     state = {
@@ -53,39 +88,42 @@ class Login extends Component {
         const { selectUsers } = this.props;
         console.log(this.props);
         return (
-            <div>
-                <h3 className='center'>WELCOME TO 🔥WOULD YOU RATHER?!🔥 </h3>
-                <FormControl onSubmit={this.handleOnSubmit} className="form-control">
-                    <InputLabel id="demo-controlled-open-select-label">Select User</InputLabel>
-                    <Select
-                        labelId="demo-controlled-open-select-label"
-                        id="demo-controlled-open-select"
-                        open={open}
-                        onClose={this.handleClose}
-                        onOpen={this.handleOpen}
-                        value={loggedInUser}
-                        onChange={this.handleChange}
-                    >
-                    <MenuItem value="">
-                        <em>None</em>
-                    </MenuItem>
-                        {selectUsers.map((user) => (
-                            <MenuItem
-                            value={user.id}
-                            key={user.id}
-                            label={user.name}
-                            onClick={(event) => this.handleChange(event)}
-                            >{user.name}</MenuItem>
-                        ))}
-                    </Select>
-                    <Button
-                        type="submit"
-                        onClick={(event) => this.handleOnSubmit(event)}
+            <FormWrapper>
+                <H3>🔥WOULD YOU RATHER?!🔥 </H3>
+                <StylesProvider injectFirst>
+                    <StyledFormControl onSubmit={this.handleOnSubmit} className="form-control">
+                        <InputLabel id="demo-controlled-open-select-label">Select User</InputLabel>
+                        <Select
+                            labelId="demo-controlled-open-select-label"
+                            id="demo-controlled-open-select"
+                            open={open}
+                            onClose={this.handleClose}
+                            onOpen={this.handleOpen}
+                            value={loggedInUser}
+                            onChange={this.handleChange}
                         >
-                        Login
-                    </Button>
-                </FormControl>
-            </div>
+                        <MenuItem value="">
+                            <em>None</em>
+                        </MenuItem>
+                            {selectUsers.map((user) => (
+                                <MenuItem
+                                value={user.id}
+                                key={user.id}
+                                label={user.name}
+                                onClick={(event) => this.handleChange(event)}
+                                >{user.name}</MenuItem>
+                            ))}
+                        </Select>
+                        <StyledButton
+                            type="submit"
+                            onClick={loggedInUser !== "" && ((event) => this.handleOnSubmit(event))}
+                            disabled={loggedInUser === "" ? true : false}
+                            >
+                            Login
+                        </StyledButton>
+                    </StyledFormControl>
+                </StylesProvider>
+            </FormWrapper>
         );
     };
 }
